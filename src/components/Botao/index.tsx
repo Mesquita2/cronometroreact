2f6b528b6ebd099fa8468738975794a29be168c4
Mesquita2 <lmesquistas@gmail.com>
2025-05-15 10:17:58 -0300

    finalizado Cronometro

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -4,17 +4,16 @@ import style from './Botao.module.scss'
 interface BotaoProps {
     children: React.ReactNode;
     type?: 'button' | "submit" | "reset" | undefined;
+    onClick?: () => void;
 }
 
-class Botao extends React.Component<BotaoProps>{
-    render(){
-        const { type = 'button'} = this.props; 
-        return(
-            <button type={type} className={style.botao}>
-                {this.props.children}
-            </button>
-        )
-    }
+function Botao({ onClick, type, children}: BotaoProps){
+    return(
+        <button onClick={onClick} type={type} className={style.botao}> 
+            {children}
+        </button>
+    )
 }
+
 
 export default Botao; 
--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -4,12 +4,14 @@ import style from "./Cronometro.module.scss";
 import { tempoParaSegundos } from "../../common/utils/time";
 import { ITarefas } from "../../types/tarefa";
 import { useEffect, useState } from "react";
+import { renderHook } from "@testing-library/react";
 
 interface Props {
     selecionado : ITarefas | undefined 
+    finalizarTarefa: () => void
 }
 
-export default function Cronometro({ selecionado }: Props) {
+export default function Cronometro({ selecionado, finalizarTarefa }: Props) {
     const [tempo, setTempo] = useState<number>();
 
     useEffect(() => {
@@ -17,13 +19,23 @@ export default function Cronometro({ selecionado }: Props) {
             setTempo(tempoParaSegundos((selecionado.tempo)))
     },[selecionado])
 
+    function regressiva(contador: number = 0){
+        setTimeout(() => {
+            if(contador > 0){
+                setTempo(contador - 1);
+                return regressiva(contador - 1);
+            }
+            finalizarTarefa();
+        }, 1000)
+    }
+
     return(
         <div className={style.cronometro}>
             <p className={style.titulo}>Escolha um card e inicie o Cronômetro</p>
             <div className={style.relogioWrapper}>
                 <Relogio tempo={tempo} />
             </div>
-            <Botao>
+            <Botao onClick={() => regressiva(tempo)}>
                 Começar
             </Botao>
         </div>
